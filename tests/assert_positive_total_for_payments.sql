0e4d611825909f4664fb3e9cc817db4056d7ac23
Alameen Waziri <wazirial@outlook.com>
2024-09-23 12:28:46 +0000

    built and added generic and singular tests to the staging models

---
+++ tests/assert_positive_total_for_payments.sql
@@ -0,0 +1,8 @@
+-- Refunds have a negative amount, so the total amount should always be >= 0.
+-- Therefore return records where this isn't true to make the test fail.
+select
+    order_id,
+    sum(amount) as total_amount
+from {{ ref('stg_stripe__payments') }}
+group by 1
+having total_amount > 0
